d5598fc00238cda5f74f8507860ddda1747ef238
LuisCarlosAlvaradoG <luisalvaradog00@gmail.com>
2026-04-20 18:54:24 -0600

    Clase01 S13 G1

--- Scripts_Clase1/18_Archivos_Pandas.py
+++ Scripts_Clase1/18_Archivos_Pandas.py
@@ -17,8 +17,30 @@ import pandas as pd
 # ---------------------------------------------------------------
 # B) SERIES Y DATAFRAMES
 # ---------------------------------------------------------------
+s = pd.Series([10, 20, 30], index = ["a", "b", "c"]) # Estructura básica
+print(s)
 
-# Acceder a elementos del DataFrame de la columna age
+df = pd.DataFrame({
+     "id" : [1, 2, 3],
+     "nombre" : ["Alina", "Beto", "Adri"],
+     "edad" : [19, 19, 18]
+    })
+print(df)
+
+# Asignar nombres a las filas
+df.index = ["fila1", "fila2", "fila3"] # Tratar de no cambiar los nombres de las filas
+print(df)
+
+# Acceder a elementos del DataFrame de la columna edad
+df.edad
+df["edad"]
+df.loc[:, "edad"] # Con .loc hacemos referencia a las columnas/filas por su nombre
+df.iloc[:, 2] # Con .iloc hacemos referencia a las columnas/filas por su indice
+
+df.iloc[1 , 1:] # Beto 19
+df.loc["fila2", ["nombre", "edad"]] # Beto 19
+
+df.loc[:, "nombre"]
 
 # ---------------------------------------------------------------
 # C) READ/WRITE CSV (DATASETS PRE-CARGADOS)
@@ -35,20 +57,49 @@ def create_students_csv(path):
     with open(path, "w", encoding="utf-8") as f:
         for r in rows: f.write(r+"\n")
 
+create_students_csv("students.csv")
+df = pd.read_csv("students.csv") # Para leer un excel tipo csv ya existente
+df.head(2)
+
+df.to_csv("students_out.csv", index= False) # Guardar de DataFrame a Excel
 
 # ---------------------------------------------------------------
 # D) SELECCIÓN, FILTRADO, ASIGNACIÓN; NA/ASTYPE
 # ---------------------------------------------------------------
+df["promedio"] = (df["math"] + df["prog"]) / 2
+# Mover la columna promedio a la posición 3
+cols = df.columns.tolist()
+cols.insert(3, cols.pop(cols.index("promedio"))) # Petición de Uriel
+df = df[cols]
+df
 
+df["status"] = np.where(df["promedio"] >= 80, "OK", np.where(df["promedio"] < 60, "REPROBADO", "MEJORAR"))
+df
 
-
+df["city"] = df["city"].fillna("Faltante") # Para llenar los datos nulos
+df
 # ---------------------------------------------------------------
 # E) value_counts, sort_values, describe
 # ---------------------------------------------------------------
+df["status"].value_counts()
 
+df.sort_values(by = "promedio", ascending= False) # Reordena el DataFrame
+
+df.describe()
 # ---------------------------------------------------------------
 # F) GROUPBY + AGG; PIVOT_TABLE
 # ---------------------------------------------------------------
+df.groupby("city") # Queda como un objeto
+df.groupby("city")["age"].mean()
+
+df.groupby("city").get_group("Guadalajara")
+df[df["city"] == "Guadalajara"] # Equivalente al groupby anterior
+
+df.groupby("city")["age"].max()
+
+df.groupby("city")["city"].value_counts() / len(df) * 100
+# Para agregarle el signo de porcentaje a cada valor, se puede usar el método .apply() con una función lambda:
+(df.groupby("city")["city"].value_counts() / len(df) * 100).apply(lambda x: f"{x:.2f}%") 
 
 # ---------------------------------------------------------------
 # G) MERGE/JOIN Y CONCAT
@@ -62,6 +113,18 @@ def create_sales_customers_csv(sales_path, customers_path):
         for r in crows: f.write(r+"\n")
 
 create_sales_customers_csv("sales.csv","customers.csv")
+
+sales = pd.read_csv("sales.csv")
+customers = pd.read_csv("customers.csv")
+print(sales)
+print(customers)
+
+# Merge
+merged = pd.merge(sales, customers, on = "cid", suffixes= ("_sales", "_cust"))
+print(merged)
+
+# Concat
+pd.concat([sales.head(2), sales.tail(1)], ignore_index= True) # Concat necesita que los df sean idénticos en sus columnas
 
 # ---------------------------------------------------------------
 # H) INTEROPERABILIDAD PANDAS + NUMPY
